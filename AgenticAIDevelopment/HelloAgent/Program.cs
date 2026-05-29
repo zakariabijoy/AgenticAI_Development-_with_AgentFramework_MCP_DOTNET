195f36448af0bf016590ad889d2be391b8fb6c1d
zakariabijoy <zakaria.bijoy@live.com>
2026-05-30 03:01:24 +0600

    Setup Development Environment: Preparing the .NET Console App

---
+++ AgenticAIDevelopment/HelloAgent/Program.cs
@@ -0,0 +1,26 @@
+﻿using Azure.AI.OpenAI;
+using Microsoft.Agents.AI;
+using Azure.Identity;
+using OpenAI.Chat;
+using System.ClientModel;
+
+// 1. Define the variables we extracted from Microsoft Foundry
+var endpoint = Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT") ?? throw new InvalidOperationException("AZURE_OPENAI_ENDPOINT environment variable is not set.");
+var model = Environment.GetEnvironmentVariable("AZURE_OPENAI_DEPLOYMENT_NAME") ?? "gpt-5-mini";
+
+// 2. Create the Agent Using MAF
+AIAgent agent = new AzureOpenAIClient(
+    new Uri(endpoint),
+    new AzureCliCredential())
+    .GetChatClient(model)
+    .AsAIAgent(instructions: "You are a friendly AI assistant. Keep your answers brief.");
+
+//Alternative way to create the agent using the api key
+//AIAgent agent = new AzureOpenAIClient(
+//    new Uri(endpoint),
+//    new ApiKeyCredential(apiKey))
+//    .GetChatClient(model)
+//    .AsAIAgent(instructions: "You are a friendly AI assistant. Keep your answers brief.");
+
+// 3. Invoke the Agent
+Console.WriteLine(await agent.RunAsync("What is the largest city in France?"));
